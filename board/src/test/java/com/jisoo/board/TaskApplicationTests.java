1047d7e7894fbbe4314ef1fac42f51ff8f3ca397
wltn2 <wltn2@DESKTOP-AVOAFT4>
2026-01-15 18:37:17 +0900

    create project

---
+++ board/src/test/java/com/jisoo/board/TaskApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jisoo.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}

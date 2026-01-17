55ab77f7bfbadad6f6bc7c04d8fd63f12958487f
wltn2 <wltn2@DESKTOP-AVOAFT4>
2026-01-18 02:31:52 +0900

    feat: implement login

--- board/src/main/java/com/jisoo/board/mapper/UserMapper.java
+++ board/src/main/java/com/jisoo/board/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import com.jisoo.board.domain.UserVo;
+
 @Mapper
 public interface UserMapper {
 	 @Select("select 1 from dual")
@@ -11,4 +13,5 @@ public interface UserMapper {
 	 
 	 public int checkId(@Param("userId") String userId);
 	 public boolean insertUser(@Param("userId") String userId, @Param("userPw") String userPw, @Param("nickname") String nickname);
+	 public UserVo findByLoginId(@Param("loginId") String loginId);
 }

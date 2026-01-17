7831d5f6fddc8278dc8b933ae2933e405a2ef2a7
wltn2 <wltn2@DESKTOP-AVOAFT4>
2026-01-17 09:19:49 +0900

    feat: add tbl_user colums

--- board/src/main/java/com/jisoo/board/domain/UserVo.java
+++ board/src/main/java/com/jisoo/board/domain/UserVo.java
@@ -4,7 +4,12 @@ import lombok.Data;
 
 @Data
 public class UserVo {
-	private String id;
-	private String pw;
-	
+	private Long userId;
+	private String loginId;
+	private String password;
+	private String nickname;
+	private String role;
+	private java.time.LocalDateTime createdAt;
+	private java.time.LocalDateTime updatedAt;
+	private java.time.LocalDateTime deletedAt;
 }
